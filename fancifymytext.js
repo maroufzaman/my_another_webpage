3a53555e758dd4e99f6724b69f954f50d9e87d11
Marouf Zaman <89886230+maroufzaman@users.noreply.github.com>
2022-09-16 18:04:04 -0700

    Update fancifymytext.js

--- fancifymytext.js
+++ fancifymytext.js
@@ -4,7 +4,7 @@ var cont = document.getElementById("text");
 
 document.getElementById("button1").addEventListener("click", function() {
     alert("Hello World!");
-    cont.style.fontSize = 24;
+    cont.style.fontSize = 24pt;
   });
 
   document.getElementById("button2").addEventListener("click", function() {
