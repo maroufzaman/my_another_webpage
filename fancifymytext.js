77d2bb6393a3cfd76e9f06560bca24b6e085e24a
Marouf Zaman <89886230+maroufzaman@users.noreply.github.com>
2022-09-18 21:11:15 -0700

    Add files via upload

--- fancifymytext.js
+++ fancifymytext.js
@@ -3,16 +3,18 @@ var cont = document.getElementById("text");
 
 
 document.getElementById("button1").addEventListener("click", function() {
-    alert("Hello World!");
-    cont.style.fontSize = "x-large";
+    alert("Hello, World!");
+    cont.style.fontSize = "4em";
   });
 
   document.getElementById("button2").addEventListener("click", function() {
-    alert("Hello World!");
-    cont.toUppperCase();
-    var parts = cont.split(" ");
-    cont = parts.join("_MOO"); 
+    alert("Hello, World!");
+    
+    cont.value = (cont.value).split(" ");
+    
+    cont.value = (cont.value).join("_MOO"); 
 
+    cont.value.toUpperCase();
 
   });
 
@@ -21,7 +23,7 @@ document.getElementById("button1").addEventListener("click", function() {
 
   
 document.getElementById("FancyShmancy").addEventListener("click", function() {
-    alert("Hello World!");
+    alert("Hello, World!");
     cont.style.fontWeight = "bold";
     cont.style.color = "blue"; 
     cont.style.textDecoration = "underline";
@@ -29,10 +31,17 @@ document.getElementById("FancyShmancy").addEventListener("click", function() {
 
   
   document.getElementById("BoringBetty").addEventListener("click", function() {
-    alert("Hello World!");
+    alert("Hello, World!");
     cont.style.fontWeight = "normal";
     cont.style.color = "black"; 
     cont.style.textDecoration = "none";
 
     
   });
+
+
+
+
+
+
+  
